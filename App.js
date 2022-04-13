b3acfacb927b080abfd954d71e84962354154d77
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-13 15:21:36 +0800

    Initial try

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts, Inter_700Bold } from "@expo-google-fonts/inter";
+import Header from "./src/components/Header";
 
 export default function App() {
+  useFonts({ Inter_700Bold });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Text style={styles.text}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fef6e4",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#001858",
+    fontSize: 20,
+    fontFamily: "Inter_700Bold",
   },
 });
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Header() {
+  return <Text>Hello from Header</Text>;
+}
