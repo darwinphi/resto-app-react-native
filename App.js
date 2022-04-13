a532f6d696e6a9431b15288847997f7f0d91c6a4
Darwin Manalo <darwinmanalophi@gmail.com>
2022-04-13 15:30:56 +0800

    Add header styling

--- App.js
+++ App.js
@@ -7,24 +7,23 @@ export default function App() {
   useFonts({ Inter_700Bold });
 
   return (
-    <View style={styles.container}>
+    <View>
       <Header />
-      <Text style={styles.text}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fef6e4",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#001858",
-    fontSize: 20,
-    fontFamily: "Inter_700Bold",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fef6e4",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     color: "#001858",
+//     fontSize: 20,
+//     fontFamily: "Inter_700Bold",
+//   },
+// });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,24 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Header() {
-  return <Text>Hello from Header</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.lightHeader}>Grab your</Text>
+      <Text style={styles.boldHeader}>Delicous Meal!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+    marginHorizontal: 25,
+  },
+  lightHeader: {
+    fontSize: 35,
+  },
+  boldHeader: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
